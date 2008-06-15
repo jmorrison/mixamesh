1b90accaa68ad11bd9c0db7c89bb06c3c18689df
John Connors <johnc@yagc.ndo.co.uk>
2008-06-15 11:07:00 +0100

    Seperated macros and invocations into two files. Fixed minor problems that prevented compilation.

---
+++ mesh-expander.lisp
@@ -0,0 +1,121 @@
+(in-package :mixamesh)
+
+;; macros to expand into a specific mesh type
+
+(def-tuple-type triangle 
+    :tuple-element-type (unsigned-byte 16) 
+    :elements (a b c))
+
+;; mesh definitons mirror ogl definitions
+
+
+(defclass base-mesh ()
+  ((id :reader id-of :initform (get-universal-time))
+   (vertex-array :accessor vertices-of :initform (make-vertex3d-array 0 :adjustable t :fill-pointer 0))
+   (face-array :accessor faces-of :initform (make-triangle-array 0 :adjustable t :fill-pointer 0))
+   (current-vertex-index :accessor current-vertex-index-of :initform 0)
+   (current-face-index :accessor current-face-index-of :initform 0))
+  (:metaclass closer-mop:funcallable-standard-class)
+  (:documentation "Base mixin class for mesh"))
+
+(defclass mesh (base-mesh)
+  ((normals-array :accessor normals-of :initform (make-vector3d-array 0 :adjustable t :fill-pointer 0))   
+   (face-normals :accessor face-normals-of :initform (make-vector3d-array 0 :adjustable t :fill-pointer 0)))
+   (:metaclass closer-mop:funcallable-standard-class)
+   (:documentation "Generic mesh type"))
+
+(defmethod has-normal-attribute-p ((mesh mesh))
+  t)
+
+(defmethod has-color-attribute-p ((mesh mesh))
+  nil)
+
+(defmethod has-texcoord-attribute-p ((mesh mesh))
+  nil)
+
+(defun expand-mesh-class-attributes (attributes)
+  "Expand the class slot defintion of the mesh attribute"
+  (loop
+     for (array-name accessor-name type-name) in attributes
+     collect 
+       `(,(cl-tuples::make-adorned-symbol array-name :suffix "ARRAY")
+          :accessor ,accessor-name          
+          :initform (,(cl-tuples::tuple-symbol type-name :def-tuple-array-maker) 0 :adjustable t :fill-pointer 0))))
+
+(defun make-accessor-symbol (sym)
+  "Given a symbol, suffix it with -OF so as to make an accessor name"
+  (cl-tuples::make-adorned-symbol sym :suffix "OF"))
+
+(defun expand-mesh-setters (name attributes)
+  "Expands the clauses used to "
+  (declare (ignorable name))
+  (loop
+     for (array-name accessor-name type-name) in attributes
+     collect
+       `(,(cl-tuples::make-adorned-symbol array-name :prefix "SET" :package :keyword)
+          (setf (,(cl-tuples::tuple-symbol type-name :def-tuple-aref)
+                  (,accessor-name mesh)
+                  (current-vertex-index-of mesh)) 
+                  (,type-name data)))))
+
+(defun expand-mesh-adders (name attributes)
+  "Expands the clauses used to add attribute values to a mesh."
+  (declare (ignorable name))
+  (loop
+     for (array-name accessor-name type-name) in attributes
+     collect
+       `(,(cl-tuples::make-adorned-symbol array-name :prefix "ADD" :package :keyword)
+          (,(cl-tuples::tuple-symbol type-name  :def-tuple-vector-push-extend) 
+            (,(cl-tuples::tuple-symbol type-name :def-tuple-getter) data) 
+            (,accessor-name mesh))
+          (,(cl-tuples::tuple-symbol type-name :def-tuple-array-dimensions) (,accessor-name mesh)))))
+
+
+(defun expand-mesh-builder-function (name attributes)
+  "Expands the form used to deefine the function used to build the mesh"
+  `(defmethod mesh-builder ((mesh ,name) op data)
+     (case op
+       (:set-vertex 
+        (setf (vertex3d-aref (vertices-of mesh) (current-vertex-index-of mesh)) (vertex3d data)))
+       (:set-face 
+        (setf (triangle-aref (faces-of mesh) (current-face-index-of mesh)) (triangle data)))
+       ,@(expand-mesh-setters name attributes)
+       (:add-vertex 
+        (vertex3d-vector-push-extend (vertex3d data) (vertices-of mesh))
+        (vertex3d-array-dimensions (vertices-of mesh)))
+       (:add-face
+        (triangle-vector-push-extend (triangle data) (faces-of mesh))
+        (triangle-array-dimensions (faces-of mesh)))
+       ,@(expand-mesh-adders name attributes)
+       (:face-index (setf (current-face-index-of mesh) data))
+       (:vertex-index (setf (current-vertex-index-of mesh) data)))))
+
+
+(defun expand-mesh-predicates (name attributes)
+  "Expand predicate functions for finding out what attributes this mesh has."
+  (iterate
+       (for (array-name accessor-name type-name) in attributes)
+       (collect `(defmethod ,(cl-tuples::make-adorned-symbol array-name :prefix "HAS" :suffix "ATTRIBUTE-P") ((mesh ,name))
+                   t))))
+
+(defun expand-base-mesh-predicates (name attributes)
+  (declare (ignore name))
+  (iterate
+    (for (array-name accessor-name type-name) in attributes)
+    (collect 
+        `(defmethod ,(cl-tuples::make-adorned-symbol array-name :prefix "HAS" :suffix "ATTRIBUTE-P") ((mesh mesh))
+           nil))))
+
+(defun expand-mesh-class  (name base attributes)
+  "Expands the form used to declare a custom mesh class"
+    `(defclass ,name (,base)
+       (,@(expand-mesh-class-attributes attributes))   
+       (:metaclass closer-mop:funcallable-standard-class)
+       (:documentation "Custom mesh type")))
+
+(defmacro def-mesh-type (name base &rest attributes)
+  `(progn ,(expand-mesh-class name base attributes)
+          ,(expand-mesh-builder-function name attributes)
+          ,@(expand-mesh-predicates name attributes)
+          ,@(expand-base-mesh-predicates name attributes)))
+
--- mesh.lisp
+++ mesh.lisp
@@ -7,125 +7,6 @@
 (unless (find-package :mesh-names)
   (make-package :mesh-names))
 
-(def-tuple-type triangle 
-    :tuple-element-type (unsigned-byte 16) 
-    :elements (a b c))
-
-;; mesh definitons mirror ogl definitions
-
-
-;; wishful
-
-
-(defclass base-mesh ()
-  ((id :reader id-of :initform (get-universal-time))
-   (vertex-array :accessor vertices-of :initform (make-vertex3d-array 0 :adjustable t :fill-pointer 0))
-   (face-array :accessor faces-of :initform (make-triangle-array 0 :adjustable t :fill-pointer 0))
-   (current-vertex-index :accessor current-vertex-index-of :initform 0)
-   (current-face-index :accessor current-face-index-of :initform 0))
-  (:metaclass closer-mop:funcallable-standard-class)
-  (:documentation "Base mixin class for mesh"))
-
-(defclass mesh (base-mesh)
-  ((normals-array :accessor normals-of :initform (make-vector3d-array 0 :adjustable t :fill-pointer 0))   
-   (face-normals :accessor face-normals-of :initform (make-vector3d-array 0 :adjustable t :fill-pointer 0)))
-   (:metaclass closer-mop:funcallable-standard-class)
-   (:documentation "Generic mesh type"))
-
-(defmethod has-normal-attribute-p ((mesh mesh))
-  t)
-
-(defmethod has-color-attribute-p ((mesh mesh))
-  nil)
-
-(defmethod has-texcoord-attribute-p ((mesh mesh))
-  nil)
-
-(defun expand-mesh-class-attributes (attributes)
-  "Expand the class slot defintion of the mesh attribute"
-  (loop
-     for (array-name accessor-name type-name) in attributes
-     collect 
-       `(,(cl-tuples::make-adorned-symbol array-name :suffix "ARRAY")
-          :accessor ,accessor-name          
-          :initform (,(cl-tuples::tuple-symbol type-name :def-tuple-array-maker) 0 :adjustable t :fill-pointer 0))))
-
-(defun make-accessor-symbol (sym)
-  "Given a symbol, suffix it with -OF so as to make an accessor name"
-  (cl-tuples::make-adorned-symbol sym :suffix "OF"))
-
-(defun expand-mesh-setters (name attributes)
-  "Expands the clauses used to "
-  (declare (ignorable name))
-  (loop
-     for (array-name accessor-name type-name) in attributes
-     collect
-       `(,(cl-tuples::make-adorned-symbol array-name :prefix "SET" :package :keyword)
-          (setf (,(cl-tuples::tuple-symbol type-name :def-tuple-aref)
-                  (,accessor-name mesh)
-                  (current-vertex-index-of mesh)) 
-                  (,type-name data)))))
-
-(defun expand-mesh-adders (name attributes)
-  "Expands the clauses used to add attribute values to a mesh."
-  (declare (ignorable name))
-  (loop
-     for (array-name accessor-name type-name) in attributes
-     collect
-       `(,(cl-tuples::make-adorned-symbol array-name :prefix "ADD" :package :keyword)
-          (,(cl-tuples::tuple-symbol type-name  :def-tuple-vector-push-extend) 
-            (,(cl-tuples::tuple-symbol type-name :def-tuple-getter) data) 
-            (,accessor-name mesh))
-          (,(cl-tuples::tuple-symbol type-name :def-tuple-array-dimensions) (,accessor-name mesh)))))
-
-
-(defun expand-mesh-builder-function (name attributes)
-  "Expands the form used to deefine the function used to build the mesh"
-  `(defmethod mesh-builder ((mesh ,name) op data)
-     (case op
-       (:set-vertex 
-        (setf (vertex3d-aref (vertices-of mesh) (current-vertex-index-of mesh)) (vertex3d data)))
-       (:set-face 
-        (setf (triangle-aref (faces-of mesh) (current-face-index-of mesh)) (triangle data)))
-       ,@(expand-mesh-setters name attributes)
-       (:add-vertex 
-        (vertex3d-vector-push-extend (vertex3d data) (vertices-of mesh))
-        (vertex3d-array-dimensions (vertices-of mesh)))
-       (:add-face
-        (triangle-vector-push-extend (triangle data) (faces-of mesh))
-        (triangle-array-dimensions (faces-of mesh)))
-       ,@(expand-mesh-adders name attributes)
-       (:face-index (setf (current-face-index-of mesh) data))
-       (:vertex-index (setf (current-vertex-index-of mesh) data)))))
-
-
-(defun expand-mesh-predicates (name attributes)
-  "Expand predicate functions for finding out what attributes this mesh has."
-  (iterate
-       (for (array-name accessor-name type-name) in attributes)
-       (collect `(defmethod ,(cl-tuples::make-adorned-symbol array-name :prefix "HAS" :suffix "ATTRIBUTE-P") ((mesh ,name))
-                   t))))
-
-(defun expand-base-mesh-predicates (name attributes)
-  (declare (ignore name))
-  (iterate
-    (for (array-name accessor-name type-name) in attributes)
-    (collect 
-        `(defmethod ,(cl-tuples::make-adorned-symbol array-name :prefix "HAS" :suffix "ATTRIBUTE-P") ((mesh mesh))
-           nil))))
-
-(defun expand-mesh-class  (name base attributes)
-  "Expands the form used to declare a custom mesh class"
-    `(defclass ,name (,base)
-       (,@(expand-mesh-class-attributes attributes))   
-       (:metaclass closer-mop:funcallable-standard-class)
-       (:documentation "Custom mesh type")))
-
-(defmacro def-mesh-type (name base &rest attributes)
-  `(progn ,(expand-mesh-class name base attributes)
-          ,(expand-mesh-builder-function name attributes)
-          ,@(expand-mesh-predicates name attributes)
-          ,@(expand-base-mesh-predicates name attributes)))
 
 (def-mesh-type wire-mesh base-mesh (colours colours-of colour))
 (def-mesh-type textured-mesh mesh (texcoord uvs-of vector2d))
@@ -149,16 +30,22 @@
      ((vertex-a vertex3d (ax ay az aw))
       (vertex-b vertex3d (bx by bz bw))
       (vertex-c vertex3d (cx cy cz cw)))
-   (vector3d-normal
-    (vector3d-cross 
-     (delta-vector3d vertex-a vertex-b)
-     (delta-vector3d vertex-a vertex-c))))
+  "return the normal of a face"
+  (:return vector3d
+           (vector3d-normal
+            (vector3d-cross 
+             (delta-vector3d vertex-a vertex-b)
+             (delta-vector3d vertex-a vertex-c)))))
 
 (def-tuple-op vector3d-sum 
      ((vector-a vector3d (ax ay az))
       (vector-b vector3d (bx by bz)))
-   (vector3d-tuple (+ ax bx) (+ ay by) (+ az bz)))
+  "return the sum of two vectors"
+  (:return vector3d
+           (vector3d-tuple (+ ax bx) (+ ay by) (+ az bz))))
 
+;; declarations to allow the iterate macro to iterate over the
+;; triangles in a mesh or the vertices
 (defclause-sequence in-triangles index-of-triangle 
   :access-fn 'triangle-aref
   :size-fn 'triangle-array-dimensions
@@ -201,10 +88,6 @@
                                 (vector3d-aref (face-normals-of self) face-index))))
           (setf (vector3d-aref vertex-normals  index) (vertex3d normal)))))
     (setf (normals-of self) vertex-normals)))
-
-
-
-
 
 
 ;; mesh geometry calculation ---------------------------------------------
@@ -254,18 +137,18 @@
       (for index index-of-vertex (vertices-of self))
       (with-vertex3d 
        (vertex3d-aref (vertices-of self) index)
-       (x y z)
-       (setf (vertex3d-aref (vertices-of self) index) (vertex3d-tuple (* x scale) (* y scale) (* z scale) 1.0)))))))
+       (x y z w)
+       (setf (vertex3d-aref (vertices-of self) index) (vertex3d-tuple (* x scale) (* y scale) (* z scale) w)))))))
 
 
 (defmethod stripify ((self mesh))
   "Stripify mesh")
 
-(defmethod decompilation ((self compiled-mesh))
-  "Create a modifiable mesh from a compiled mesh")
+;; (defmethod decompilation ((self compiled-mesh))
+;;   "Create a modifiable mesh from a compiled mesh")
 
-(defmethod compilation ((self mesh))
-  "Given a mesh return a compiled mesh, which is a non-modifiable mesh optimised for rendering in foreign memory."
-)
+;; (defmethod compilation ((self mesh))
+;;   "Given a mesh return a compiled mesh, which is a non-modifiable mesh optimised for rendering in foreign memory."
+;; )
 
 
